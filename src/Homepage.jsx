85747eacfb35f3de6a27288d106d78f1c534a8b7
Raymond Akalonu <raymondosy1@gmail.com>
2019-10-10 12:15:17 +0100

    setup application add homepage component

---
+++ src/Homepage.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Homepage.scss";
+
+const Homepage = () => {
+  return (<div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATS</h1>
+          <span classsName="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATS</h1>
+          <span classsName="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">JACKETS</h1>
+          <span classsName="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">WOMENS</h1>
+          <span classsName="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">MENS</h1>
+          <span classsName="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>)
+};
+
+export default Homepage;
