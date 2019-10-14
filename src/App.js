47188c0f10fde3d28aaacc6e34773347f5b5e988
Raymond Akalonu <raymondosy1@gmail.com>
2019-10-14 13:57:41 +0100

    - add the react-router-dom package for routing - use the HOC withRouter for routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage/Homepage';
+import './App.css';
+
+const HatsPage = () => (
+  <div>
+    <h1>Welcome to Hats page!</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage/>
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route exact path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
