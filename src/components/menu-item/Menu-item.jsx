7bff28d4edb14bb48f2e67262d7575227772cb7d
Raymond Akalonu <raymondosy1@gmail.com>
2019-10-14 01:12:12 +0100

    complete styling menu cards

--- src/components/menu-item/Menu-item.jsx
+++ src/components/menu-item/Menu-item.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import './Menu-item.scss';
 
 const MenuItem = ({ title, imageUrl, size }) => (
-    <div style={{
-        backgroundImage: `url(${imageUrl})`
-    }} className={`${size} menu-item`}>
+    <div className={`${size} menu-item`}>
+        <div
+            className="background-image"
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }}
+        />
         <div className="content">
-        <h1 className="title">{title.toUpperCase()}</h1>
-        <span classsName="subtitle">SHOP NOW</span>
+            <h1 className="title">{title.toUpperCase()}</h1>
+            <span classsName="subtitle">SHOP NOW</span>
         </div>
     </div>
 );
