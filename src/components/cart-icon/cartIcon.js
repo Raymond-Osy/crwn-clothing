d2c9128437fcdba9040900275a20ea758b7ca2ef
Raymond Akalonu <raymondosy1@gmail.com>
2019-10-31 01:21:52 +0100

    add redux selector to update count fot cart items dynamically

--- src/components/cart-icon/cartIcon.js
+++ src/components/cart-icon/cartIcon.js
@@ -4,10 +4,10 @@ import { toggleCartHidden } from "../../redux/cart/cartActions";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import "./cartIcon.scss";
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
 
@@ -15,4 +15,8 @@ const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = ({cart : {cartItems}}) => ({
+  itemCount: cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
